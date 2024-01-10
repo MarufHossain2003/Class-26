6b487721665318ebf7fbff57c4f884dc8473285a
MarufHossain2003 <kmaruf2003@gmail.com>
2024-01-10 23:11:11 +0600

    Example of JS if else condition...

---
+++ script.js
@@ -0,0 +1,59 @@
+
+// practice 
+var marks = 69;
+
+
+if (marks < 101 && marks > 79){
+    console.log('Your Grade is A+')
+} 
+else if(marks < 80 && marks > 69){ 
+    console.log('Your Grade is A')
+} 
+else if(marks < 70 && marks > 59){ 
+    console.log('Your Grade is B')
+} 
+else if(marks < 60 && marks > 49){ 
+    console.log('Your Grade is C')
+} 
+else if(marks < 50 && marks > 39){ 
+    console.log('Your Grade is D')
+} 
+else if(marks < 40 && marks > 32){ 
+    console.log('Your Grade is D')
+} 
+else { 
+    console.log('You are not promoted.')}
+
+
+// Home work practice (1)
+
+let number1 = 62;
+let number2 = 39;
+
+if (number1>number2){
+        console.log('Large number is '+ number1)
+    }
+else if (number2>number1){
+        console.log('Large number is '+ number1)
+    }
+else {
+        console.log('Number are both same.')
+    }
+
+// Home work practice (2)
+
+let number = -4;
+
+if (number > 0){
+     console.log('It is a "Positive" number.')
+    }
+else if (number < 0){
+     console.log('It is a "Negetive" number.')
+    }
+else {
+     console.log('It is "Zero".')
+    }
+
+// Home work practice (3)
+
+
